75bf4068ce264b5cbdf04e480447e9ea115f2ece
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2023-05-26 18:41:27 +0200

    fixed bugs
    fixed bugs

--- Exam2_project 3/src/Pages/Authentication.js
+++ Exam2_project 3/src/Pages/Authentication.js
@@ -98,14 +98,14 @@ const Authentication = () => {
     );
   }
   return (
-    <div className="w-[100%] min-h-screen flex flex-col lg:flex-row justify-center items-center gap-[2rem] lg:gap-[5rem] py-[2rem] lg:py-0 px-[12px] lg:px-0">
+    <div className="w-[90%] min-h-screen flex flex-col lg:flex-row justify-center items-center gap-[2rem] lg:gap-[5rem] py-[2rem] lg:py-0 px-[12px] lg:px-0">
       <div className="lg:w-[600px] p-[22px] rounded-[8px] border border-black">
         <h1 className="text-[48px] lg:text-[76px] text-center font-bold">
           Sign-in
         </h1>
         <form
           onSubmit={handleSignin}
-          className="flex flex-col gap-[42px] mt-[50px]"
+          className="flex flex-col  gap-[42px] mt-[50px]"
         >
           <div className="flex flex-col">
             <label
--- Exam2_project 3/src/Pages/SignUp.js
+++ Exam2_project 3/src/Pages/SignUp.js
@@ -4,12 +4,14 @@ import { useNavigate } from "react-router-dom";
 const SignUp = () => {
   const navigate = useNavigate();
 
-  
-
   return (
-    <div className="w-[100%] flex justify-center items-center px-[12px] lg:px-0 py-[3rem]">
-      
+    <>
+      <h1>Sign in or Sign up</h1>
+      <div className="w-[100%] flex justify-center items-center px-[12px] lg:px-0 py-[3rem]">
     </div>
+    </>
+   
+    
   );
 };
 
