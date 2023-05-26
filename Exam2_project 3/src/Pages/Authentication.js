a419880fd579f1241d94ef481923eb901d67396e
Fariaddd <89278138+Fariaddd@users.noreply.github.com>
2023-05-26 23:26:23 +0200

    fixed bugs
    fixed bugs

--- Exam2_project 3/src/Pages/Authentication.js
+++ Exam2_project 3/src/Pages/Authentication.js
@@ -92,13 +92,13 @@ const Authentication = () => {
   };
   if (loading) {
     return (
-      <div className="w-[10%] min-h-screen flex flex-col lg:flex-row justify-center items-center gap-[2rem] lg:gap-[5rem] py-[2rem] lg:py-0 px-[12px] lg:px-0">
+      <div className="w-[100%] min-h-screen flex flex-col lg:flex-row justify-center items-center gap-[2rem] lg:gap-[5rem] py-[2rem] lg:py-0 px-[12px] lg:px-0">
         <InfinitySpin width="200" color="#4fa94d" />
       </div>
     );
   }
   return (
-    <div className="w-[90%] min-h-screen flex flex-col lg:flex-row justify-center items-center gap-[2rem] lg:gap-[5rem] py-[2rem] lg:py-0 px-[12px] lg:px-0">
+    <div className=" flex flex-col lg:flex-row justify-center items-center  py-[2rem] px-[12px] ">
       <div className="lg:w-[600px] p-[22px] rounded-[8px] border border-black">
         <h1 className="text-[48px] lg:text-[76px] text-center font-bold">
           Sign-in
@@ -142,7 +142,7 @@ const Authentication = () => {
           </div>
         </form>
       </div>
-      <div className="lg:w-[600px] p-[22px] rounded-[8px] border border-black">
+      <div className="lg:w-[600px] m-auto  p-[22px] rounded-[8px] border border-black">
         <h1 className="text-[32px] lg:text-[76px] text-center font-bold">
           Sign-up
         </h1>
